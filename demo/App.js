0c2b40bb811c8f532f62a1f0057b5f80cb0880fe
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2018-08-05 11:36:18 +0700

    add button and set state

--- demo/App.js
+++ demo/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class App extends React.Component {
 
@@ -7,14 +7,25 @@ class App extends React.Component {
     super(props);
     this.state = {
       name: 'react-native-demo',
-      welcome: 'Open up App.js to start working on your app!!'
+      welcome: 'Open up App.js to start working on your app!!',
+      age: 24
     };
+  }
+
+  onButtonClicked() {
+    this.setState((prevState, props) => {
+      return {
+        age: prevState.age++
+      };
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>{this.state.welcome}</Text>
+        <Text>your age is {this.state.age}</Text>
+        <Button onPress={onButtonClicked} title="Add my age"/>
       </View>
     );
   }
